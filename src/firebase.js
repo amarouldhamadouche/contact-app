4620a99c4272f1e52921cfc0a3bb8c5169971315
amar <66710686+amarouldhamadouche@users.noreply.github.com>
2022-08-26 14:28:50 +0100

    Create firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,7 @@ import { getDatabase, ref } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCVZUMi0fhYhL8bN7sOv6HMD9DXe4urAkI",
-  authDomain: "secondproject-25a4f.firebaseapp.com",
-  projectId: "secondproject-25a4f",
-  storageBucket: "secondproject-25a4f.appspot.com",
-  messagingSenderId: "890033868178",
-  appId: "1:890033868178:web:08ec679c74b4a50014054b",
-  measurementId: "G-XW61Y3YY0B",
+ur firebase configuration
 };
 
 // Initialize Firebase
